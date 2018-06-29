cc32361b21c9fd724d0b366a18d81764e18a2c51
Nirut Tothong <mumuza001@hotmail.com>
2018-06-29 18:28:53 +0700

    +

--- register_all.php
+++ register_all.php
@@ -14,8 +14,6 @@ if(isset($_POST["submit"])){
 		if($rs->log_user_type == "sender") { header("location:sender/index.php"); }
 	}
 }
-
-
 ?>
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html lang="en">
